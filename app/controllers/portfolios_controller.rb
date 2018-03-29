d7216da470345bd1a2f2557d13b9554acdf190b8
Chris Leonard <chrisl@Chriss-MacBook-Pro.local>
2018-03-29 18:40:23 +0100

    added portfolio destroy

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,4 +43,19 @@ def show
 
 end
 
+  def destroy
+    #perfolorm lookup
+      @portfolio_item = Portfolio.find(params[:id])
+   # Destroy record
+      @portfolio_item.destroy
+   # Redirect
+      respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+      format.json { head :no_content }
+  end
+
+
+  end
 end
+
+
